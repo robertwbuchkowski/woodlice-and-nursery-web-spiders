acb4254d2f5d430f628533bc54783fadb8ae303c
robertwbuchkowski <robert.buchkowski@yale.edu>
2020-03-27 09:55:48 -0400

    Cleaned up HOBO data code.

--- HOBOdataanalysis.R
+++ HOBOdataanalysis.R
@@ -3,105 +3,8 @@
 library(tidyverse)
 library(lubridate)
 
-# Extract the HOBO data
-
-d1 <- read.csv("Data/HOBOdata/20.csv", skip = 1)
-d1 <- d1[,2:3]
-colnames(d1) <- c("Date", "Temp")
-d1$Date = mdy_hms(d1$Date)
-d1[,"ID"] = "20.csv"
-
-plot(Temp~Date, data= d2, type = "b")
-
-d2 <- read.csv("Data/HOBOdata/10731729.csv", skip = 1)
-d2 <- d2[,2:3]
-colnames(d2) <- c("Date", "Temp")
-d2$Date = mdy_hms(d2$Date)
-d2$ID = "10731729.csv"
-
-lf <- list.files("Data/HOBOdata/tempdata080918/")
-
-lf2 <- vector(mode = "list", length = length(lf))
-
-for(i in 1:length(lf)){
-  
-  da <- read.csv(paste0("Data/HOBOdata/tempdata080918/",lf[i]), skip = 1)
-  da <- da[,2:3]
-  colnames(da) <- c("Date", "Temp")
-  da$Date = mdy_hms(da$Date)
-  da[,"ID"] = lf[i]
-  
-  lf2[[i]] = da
-}
-
-lf2[[24]] = d1
-lf2[[25]] = d2
-
-# Add them all together:
-
-LF <- do.call("rbind",lf2)
-
-LF %>% as_tibble() %>% filter(Date > mdy_hms("08/01/17 08:00:00")) %>% ggplot(aes(x=Date, y = Temp, color = ID)) + geom_line()
-
-
-LF %>% filter(Date < mdy_hms("08/01/17 08:00:00")) %>% dim()
-LF %>% filter(Date > mdy_hms("08/01/17 08:00:00")) %>% dim()
-
-
-# Load in the 44 data
-
-lf44 <- list.files("Data/HOBOdata/44/")
-
-lf442 <- vector(mode = "list", length = length(lf))
-
-for(i in 1:length(lf)){
-  
-  da <- read.csv(paste0("Data/HOBOdata/44/",lf[i]), skip = 1)
-  da <- da[,2:3]
-  colnames(da) <- c("Date", "Temp")
-  da$Date = mdy_hms(da$Date)
-  da[,"ID"] = lf[i]
-  
-  lf442[[i]] = da
-}
-
-LF44 <- do.call("rbind",lf442)
-
-LF44 %>% as_tibble() %>% ggplot(aes(x=Date, y = Temp, color = ID)) + geom_line()
-
-uID = unique(LF44$ID)
-
-for(i in 1:length(uID)){
-  print(paste0(uID[i], " is ", all(LF44 %>% filter(ID == uID[i]) == LF %>% filter(ID == uID[i]), na.rm = T)))
-}
-
-# LF44 and LF are the same, so only need to use 1
-# LF has one extra dataset that LF44 doesn't have and it is 20.csv
-
-# 20.csv is the same as cage20.csv, so use LF44
-LF %>% as_tibble() %>% filter(ID %in% c("cage20.csv", "20.csv")) %>% ggplot(aes(x=Date, y = Temp, color = ID, linetype = ID)) + geom_line() + theme_bw()
-
-LF44 = LF44 %>% as_tibble() %>% filter(!is.na(Temp))
-
-mac = data.frame(ID = c("10731719", "10731721","10731724","10731727",
-                        "10731723", "10731720", "10731729", "10731729_0", "10731730",
-                        "cage20","cage21","cage25","cage26","cage27",
-                        "cage29","cage35","cage38","cage44"),
-                 Cage = c("25","26","29","38","20","27", "43", "43","44",
-                          "20", "21", "25","26", "27", "29", "35", "38", "44"))
-
-LF44 = LF44 %>% separate(ID, into = c("ID", NA), sep = -4) %>%
-  left_join(mac) %>%
-  filter(!is.na(Cage)) %>% select(-ID)
-
-write_csv(LF44, "Data/HOBO.csv")
-
-
-
-# Starting analyiss with LF44
+# Load in the data
 LF44 = read_csv("Data/HOBO.csv") %>% mutate(Cage = as.character(Cage))
-
-LF44 %>% ggplot(aes(x=Date, y = Temp, color = Cage)) + geom_line() + theme_bw()
 
 # Add the treatment identifiers from other database
 LF44b = LF44  %>%
@@ -113,9 +16,7 @@ LF44b = LF44  %>%
   ) %>% mutate(ID = ifelse(Temperature == "W", "Warmed", "Control"),
                ID2 = ifelse(Isopod == "N", "Woodlouse", "No woodlouse"))
 
-LF44expt = LF44b %>% filter(Date > mdy_hms("08/09/18 07:00:00") &
-                              Date < mdy_hms("08/09/18 19:00:00"))
-
+# Add averages and sd at Cage level
 etavg = LF44b %>% group_by(Cage, Temperature) %>%
   summarize(Temp = mean(Temp)) %>% ungroup() %>% group_by(Temperature) %>%
   summarize(sd = sd(Temp), avg = mean(Temp)) %>%
@@ -124,6 +25,12 @@ etavg = LF44b %>% group_by(Cage, Temperature) %>%
 
 LF44b = LF44b %>% left_join(etavg)
 
+# Create experimental data for Aug 9th
+LF44expt = LF44b %>% filter(Date > mdy_hms("08/09/18 07:00:00") &
+                              Date < mdy_hms("08/09/18 19:00:00"))
+
+
+
 exptavg = LF44expt %>% group_by(Cage, Temperature) %>%
   summarize(Temp = mean(Temp)) %>% ungroup() %>% group_by(Temperature) %>%
   summarize(sd = sd(Temp), avg = mean(Temp))  %>%
@@ -131,6 +38,7 @@ exptavg = LF44expt %>% group_by(Cage, Temperature) %>%
 
 LF44expt = LF44expt %>% left_join(exptavg)
 
+# Plot the results
 p1 = LF44b %>% 
   ggplot(aes(x=Date, color = ID)) +  
   geom_line(aes(y = avg), size = 2) + 
